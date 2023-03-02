a5f59bf781751646604d49102540e51a930e8d05
clux01 <126171638+clux01@users.noreply.github.com>
2023-03-02 22:08:26 +0100

    Add files via upload

---
+++ mic-1-project/verilog/rtl/defines_add.sv
@@ -0,0 +1,11 @@
+`ifndef _mic1_include_
+`define _mic1_include_
+
+`define MIC1_PROGRAM "add.mem"
+`define MIC1_MICROCODE "microcode.mem"
+`define CONSTANTPOOL_ADDRESS 'h0048
+`define LOCALVARIABLEFRAME_ADDRESS 'h004b
+`define STACKPOINTER_ADDRESS 'h005b
+`define MEMORY_SIZE 'h008b
+
+`endif
